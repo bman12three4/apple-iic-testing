6e9cb4cbfa832360b7ac5213c33c1039a3a99985
bman12three4 <byron.lathi@student.lovett.org>
2019-05-15 19:23:32 -0400

    Update helloworld.asm

--- helloworld.asm
+++ helloworld.asm
@@ -3,13 +3,13 @@
 ********************************
 
 
-		org	$8000
-		ldx #$0
-loop	lda string,x; Load  the next letter of string
-		beq done	; If a loaded $00, then end. (null terminated string)
-		jsr	$FDF0	; Print subroutine
-		inx
-		bne loop	; I don't think this does anything
+	org	$8000
+	ldx 	#$0
+loop	lda 	string,x	; Load  the next letter of string
+	beq 	done		; If a loaded $00, then end. (null terminated string)
+	jsr	$FDF0		; Print subroutine
+	inx
+	bne 	loop		; I don't think this does anything
 done	rts
 string	asc	"Hello, World! Now I have variable string length!",00
-		end
+	end
