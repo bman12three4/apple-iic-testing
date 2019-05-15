a123cff4ba6bf691d743bc82f480aebff5db7b24
bman12three4 <byron.lathi@student.lovett.org>
2019-05-15 19:35:14 -0400

    Update letters.asm

--- letters.asm
+++ letters.asm
@@ -1,8 +1,8 @@
 ********************************
 *        Screen Testing        *
 ********************************
-		org	$8000	; Start at $8000
+	org	$8000	; Start at $8000
 print	equ	$FDF0	; Print subroutine
-		lda	#"A"	; "A"
-		jsr	print	; Print the character
-end		rts			; End the program
+	lda	#"A"	; "A"
+	jsr	print	; Print the character
+end	rts		; End the program
